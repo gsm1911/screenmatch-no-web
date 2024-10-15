0dbeac96040b89560ca490fdab55c28ca780b39c
Gustavo Maeda <gustavosm115@outlook.com>
2024-10-15 12:48:10 -0300

    inicio do projeto

---
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,26 @@
+package br.com.alura.screenmatch;
+
+import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.service.ConsumoAPI;
+import br.com.alura.screenmatch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoAPI();
+		String endereco = "http://www.omdbapi.com/?t=simpsons&apikey=e07ceec0";
+		var json = consumoApi.obterDados(endereco);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+	}
+}
