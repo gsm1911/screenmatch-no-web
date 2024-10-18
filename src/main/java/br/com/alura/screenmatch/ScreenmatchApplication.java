f44a3bce3c52b51ac644203cb4b926bb05c8a6b8
Gustavo Maeda <gustavosm115@outlook.com>
2024-10-18 13:30:53 -0300

    Criado as classes DadosEpisodio, DadoTemporada e Main.

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,13 +1,9 @@
 package br.com.alura.screenmatch;
 
 import br.com.alura.screenmatch.main.Main;
-import br.com.alura.screenmatch.model.DadosTemporada;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -17,7 +13,7 @@ public class ScreenmatchApplication implements CommandLineRunner {
 	}
 
 	@Override
-	public void run(String... args) throws Exception {
+	public void run(String... args) {
 		Main main = new Main();
 		main.exibeMenu();
 
